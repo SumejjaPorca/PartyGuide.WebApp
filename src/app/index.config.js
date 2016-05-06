800ad97791033deebd4cbaac07bd021f6798fe71
pejovicluka454@gmail.com <pejovicluka454@gmail.com>
2016-05-06 23:08:00 +0200

    Prijevoda malo

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -20,12 +20,25 @@
     $translateProvider.translations('en', {
       DETAILS: 'See more details...',
       HELLO: 'Hello',
+      REGISTER:"Register",
+      LOGIN:"Login",
+      REQ:"Required.",
+      PNS:"Passwords are not same.",
+      PS:"Password need to have at least 8 chars.",
+      RP:"Repeat password",
+      USERNAME:"Username",
       BUTTON_LANG_EN: 'english',
       BUTTON_LANG_BS: 'bosnian'
     });
     $translateProvider.translations('bs', {
       DETAILS: 'Pogledaj više detalja...',
       HELLO: 'Zdravo',
+      REGISTER:"Registruj se",
+      LOGIN:"Prijavi se",
+      REQ:"Obavezno.",
+      PNS:"Passwordi nisu jednaki.",
+      PS:"Password mora imati najmanje 8 znakova.",USERNAME:"Korisnicko ime",
+RP:"Ponovi password",
       BUTTON_LANG_EN: 'engleski',
       BUTTON_LANG_BS: 'bosanski'
     });
